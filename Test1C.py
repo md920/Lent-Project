dae851ba3150931fb4eed6ae3039bdebf17c0785
Manula Dharmawardana <md920@cam.ac.uk>
2022-02-09 11:39:45 +0000

    fixed tests

--- Test1C.py
+++ Test1C.py
@@ -1,10 +1,9 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
 
-def test_stations_within_radius():
-    """Test for Task 1C"""
-    stations=build_station_list()
-    centre=[52.2053,0.1218]
-    r = 1 #radius
-    station_in_radius=stations_within_radius(stations,centre, r)
-    assert station_in_radius==['Cambridge Jesus Lock']
+"""Test for Task 1C"""
+stations=build_station_list()
+centre=[52.2053,0.1218]
+r = 1 #radius
+station_in_radius=stations_within_radius(stations,centre, r)
+assert station_in_radius==['Cambridge Jesus Lock']
