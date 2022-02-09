d3815bddac1ec13581de1409a0ff886622761002
Leo Mills <lm946@cam.ac.uk>
2022-02-09 12:07:49 +0000

    fixed??

--- Task1F.py
+++ Task1F.py
@@ -2,4 +2,6 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.station import inconsistent_typical_range_stations
 
 stations = build_station_list()
-print(inconsistent_typical_range_stations(stations))
+incon = inconsistent_typical_range_stations(stations)
+print(incon)
+
