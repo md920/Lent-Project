2aacb9c84a5fd2cb9b56a5ab586bd16a2a779c88
Manula Dharmawardana <md920@cam.ac.uk>
2022-02-09 10:15:56 +0000

    Fixed task 1B

--- Task1B.py
+++ Task1B.py
@@ -5,12 +5,10 @@ stations = build_station_list()
 stortedStations = stations_by_distance(stations, (52.2053, 0.1218))
 newStats = []
 for s in stortedStations:
-    newStats.append((s[0].name, s[0].town, s[1], 6))
+    newStats.append((s[0].name, s[0].town, s[1]))
 
 print("10 closest stations:")
-for x in range(11):
-    print(newStats[x])
+print(newStats[:10])
 
 print("\n 10 furthest stations:")
-for y in range(11):
-    print(newStats[-y])
+print(newStats[-10:])
