2da9e28a241dad3c6611263a6040160aa87688c0
Manula Dharmawardana <md920@cam.ac.uk>
2022-02-09 11:39:28 +0000

    Fixed test 1B

--- Test1B.py
+++ Test1B.py
@@ -1,14 +1,12 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 
-def test_stations_by_distance():
-    """Test for Task 1B"""
-    stations = build_station_list()
-    p=[52.2053, 0.1218]
-    distance=stations_by_distance(stations,p)
-    test_list=[]
-    for i in range (len(distance)):
-        x=(distance[i][0].name, distance[i][0].town, distance[i][1])
-        test_list.append(x)
-
-    assert test_list[0]==('Cambridge Jesus Lock', 'Cambridge', 0.8402364350834995)
+"""Test for Task 1B"""
+stations = build_station_list()
+p=[52.2053, 0.1218]
+distance=stations_by_distance(stations,p)
+test_list=[]
+for i in distance:
+    x=(i[0].name,i[0].town, i[1])
+    test_list.append(x)
+assert test_list[0]==('Cambridge Jesus Lock', 'Cambridge', 0.840237595667494)
