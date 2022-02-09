9d2311dcb6ff174730aeb26ccd0c980eaa507bac
Leo Mills <lm946@cam.ac.uk>
2022-02-09 12:17:32 +0000

    tests fixed some more

--- test_1B.py
+++ test_1B.py
@@ -2,11 +2,12 @@ from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 
 """Test for Task 1B"""
-stations = build_station_list()
-p=[52.2053, 0.1218]
-distance=stations_by_distance(stations,p)
-test_list=[]
-for i in distance:
-    x=(i[0].name,i[0].town, i[1])
-    test_list.append(x)
-assert test_list[0]==('Cambridge Jesus Lock', 'Cambridge', 0.840237595667494)
+def test_stations_by_distance():
+    stations = build_station_list()
+    p=[52.2053, 0.1218]
+    distance=stations_by_distance(stations,p)
+    test_list=[]
+    for i in distance:
+        x=(i[0].name,i[0].town, i[1])
+        test_list.append(x)
+        assert test_list[0]==('Cambridge Jesus Lock', 'Cambridge', 0.840237595667494)
--- test_1C.py
+++ test_1C.py
@@ -2,10 +2,9 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
 
 """Test for Task 1C"""
-stations=build_station_list()
-centre=[52.2053,0.1218]
-r = 10 #radius
-station_in_radius=stations_within_radius(stations,centre, r)
-assert station_in_radius==['Bin Brook', 'Cambridge Baits Bite', "Cambridge Byron's Pool",
- 'Cambridge Jesus Lock', 'Comberton', 'Dernford', 'Girton',
- 'Haslingfield Burnt Mill', 'Lode', 'Oakington', 'Stapleford']
+def test_stations_within_radius():
+    stations=build_station_list()
+    centre=[52.2053,0.1218]
+    r = 10 #radius
+    station_in_radius=stations_within_radius(stations,centre, r)
+    assert station_in_radius==['Bin Brook', 'Cambridge Baits Bite', 'Cambridge Byron''s Pool', 'Cambridge Jesus Lock', 'Comberton', 'Dernford', 'Girton', 'Haslingfield Burnt Mill', 'Lode', 'Oakington', 'Stapleford']
