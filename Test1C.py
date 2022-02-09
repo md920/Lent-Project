f8f4dd7428f72edcd7e30dace23ed1b581c9eb51
Manula Dharmawardana <md920@cam.ac.uk>
2022-02-09 10:29:38 +0000

    Tests done

--- Test1C.py
+++ Test1C.py
@@ -2,6 +2,7 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
 
 def test_stations_within_radius():
+    """Test for Task 1C"""
     stations=build_station_list()
     centre=[52.2053,0.1218]
     r = 1 #radius
