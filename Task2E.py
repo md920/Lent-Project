84e584ec896bfbcb22cd91d7b60674135185618b
Leo Mills <lm946@cam.ac.uk>
2022-02-26 17:49:52 +0000

    poo poo pee pee

---
+++ Task2E.py
@@ -0,0 +1,27 @@
+from bokeh.io import show
+
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.plot import plot_water_levels, plot_water_levels_multiple
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+    high_risk_stations = stations_highest_rel_level(stations, 5)
+
+    # dt = 10
+
+    # for station in high_risk_stations:
+    #     dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days=dt))
+    #     p = plot_water_levels(station, dates, levels)
+    #     show(p)
+
+    p = plot_water_levels_multiple(high_risk_stations, dt=10)
+    show(p)
+
+
+
+if __name__ == "__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    run()
