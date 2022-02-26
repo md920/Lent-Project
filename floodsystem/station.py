5b50a8e41ef4ab073716a2fe553db831d4d4d8a5
Manula Dharmawardana <md920@cam.ac.uk>
2022-02-26 16:29:27 +0000

    Task 2A

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -50,6 +50,13 @@ class MonitoringStation:
         else:
             return True
 
+    def relative_water_level(self):
+        try:
+            difference = self.typical_range[1]- self.typical_range[0]
+            return((self.latest_level - self.typical_range[0])/difference)
+        except:
+            return(None)
+
 
 def inconsistent_typical_range_stations(stations):
     inconStat = []
