620f08fcd3a3b521ef0d9e8047a4119a869549ab
Manula Dharmawardana <md920@cam.ac.uk>
2022-02-26 18:16:48 +0000

    fix

--- Task2E.py
+++ Task2E.py
@@ -1,26 +1,21 @@
-from bokeh.io import show
-
-from floodsystem.flood import stations_highest_rel_level
-from floodsystem.plot import plot_water_levels, plot_water_levels_multiple
+import datetime
 from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.plot import plot_water_levels
+from floodsystem.flood import stations_highest_rel_level
 
 
 def run():
+    # Build list of stations
     stations = build_station_list()
+    N=5
     update_water_levels(stations)
-    high_risk_stations = stations_highest_rel_level(stations, 5)
-
-    # dt = 10
-
-    # for station in high_risk_stations:
-    #     dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days=dt))
-    #     p = plot_water_levels(station, dates, levels)
-    #     show(p)
-
-    p = plot_water_levels_multiple(high_risk_stations, dt=10)
-    show(p)
-
-
+    list_of_5_stations_greatest_level=stations_highest_rel_level(stations , N)
+    dt=10
+    for station in list_of_5_stations_greatest_level:
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_levels(station, dates, levels)
+   
 
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
